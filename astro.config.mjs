2b179769930c4adac94bf5282351e7fba7c7d575
Fer <fernando.falcon@holbertonstudents.com>
2025-06-06 11:42:44 -0300

    Remove s from url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://ferfalcon.github.io',
-    base: 'hangman-games',
+    base: 'hangman-game',
 });
